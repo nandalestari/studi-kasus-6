bf3e7f62a8b8776f12b6fb9a2ecf8d06dd62c4ee
nandalestari <nndalstari27@gmail.com>
2022-04-27 11:15:11 +0700

    studi kasus 6

---
+++ studi kasus 6.cpp
@@ -0,0 +1,10 @@
+int main(){
+	int i, mhs, A[50], jumlah, max, min;
+  float rata;
+    string dosen, matkul;
+	cout << "##         PENGINPUTAN NILAI  ##\n";
+	cout << "=========================================\n";
+	cout << endl;
+  cout << "Masukkan Jumlah Mahasiswa : ";
+  cin >> mhs;
+  cout << endl;
