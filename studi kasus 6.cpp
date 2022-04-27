c0e9ab96d2a901b60fa36a0b46655755d4af3946
BintiSholikhah <bintisholikhah95@gmail.com>
2022-04-27 11:32:08 +0700

    input

--- studi kasus 6.cpp
+++ studi kasus 6.cpp
@@ -1,10 +1,20 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
 int main(){
 	int i, mhs, A[50], jumlah, max, min;
-  float rata;
+ 	float rata;
     string dosen, matkul;
 	cout << "##         PENGINPUTAN NILAI  ##\n";
 	cout << "=========================================\n";
 	cout << endl;
-  cout << "Masukkan Jumlah Mahasiswa : ";
-  cin >> mhs;
-  cout << endl;
+	cout << "Masukkan Jumlah Mahasiswa : ";
+  	cin >> mhs;
+  	cout << endl;
+    cout << " Masukkan Nilai Mahasiswa (sesuai absen) : \n";
+    for (i=0; i<mhs; i++){
+		cout << " Mahasiswa [" << i << "] = ";
+		cin >> A[i];
+		jumlah=+jumlah + A[i];
+	}
